e4fe7c76d3ec5f94df2456cd6581adc08c30b13f
Alexander <aslesarev@inbox.ru>
2019-09-07 09:24:04 +0300

    процедуру в файл, обновил композер

---
+++ tests/unit/FirstTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
