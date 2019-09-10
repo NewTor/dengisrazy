582ab3109e56d97aadf08120d52d416c97523278
Alexander <aslesarev@inbox.ru>
2019-09-10 11:52:37 +0300

    каркас для юнит теста

--- tests/unit/SaveDataTest.php
+++ tests/unit/SaveDataTest.php
@@ -1,10 +1,42 @@
 <?php
 namespace tests\unit;
 
-class SaveDataTest extends \PHPUnit\Framework\TestCase
+use app\models\MainTable;
+use PHPUnit\Framework\TestCase;
+
+
+class SaveDataTest extends TestCase
 {
-    public function testTrue()
+    /*public function testTrue()
     {
         $this->assertTrue(true);
+    }*/
+
+
+    public function testSave()
+    {
+        //$main_table = new MainTable();
+        $post = [
+            'json' => [
+                'email' => '',
+                'fio' => '',
+                'rangeList' => '',
+            ]
+        ];
+
+        $this->assertEmpty($post);
+        return $post;
+
+        //$this->assertTrue($main_table->load($post), 'Load POST data');
+        //$this->assertTrue($main_table->saveData(), 'Save data');
     }
+
+    /*public function testEmpty()
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }*/
+
 }
